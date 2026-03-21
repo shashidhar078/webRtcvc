6cf37e8baa26304d0cb664f7e05b600cd837959a
shashidhar078 <nagunurishashidhar@gmail.com>
2026-03-21 22:34:58 +0530

    revised context-api and implemented auth context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,19 @@ import React from "react";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import LandingPage from './pages/LandingPage';
 import Authentication from './pages/Authentication';
+import {AuthProvider} from "./contexts/AuthContext.jsx";
 
 function App() {
   return (
     <div className="App">
       
     <Router>
+      <AuthProvider>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
            <Route path="/auth" element={<Authentication/>} />
       </Routes>
+      </AuthProvider>
     </Router>
 
 
